40b521a4c9cbadef9eb821be1b3d6285ea01015e
Christopher Stevens <owntheweb@gmail.com>
2016-02-08 13:15:07 -0700

    work to make app function as web page and PhoneGap app

--- js/app.js
+++ js/app.js
@@ -1,9 +1,7 @@
-document.addEventListener('deviceready', function() { 
-    //keep app awake using insomnia plugin (added in config.xml)
-    window.plugins.insomnia.keepAwake();
-
+(function(){
+	
 	var start = function() {
-	    //show a message if webGL is not supported
+    	//show a message if webGL is not supported
 		if(!Detector.webgl) {
 			Detector.addGetWebGLMessage();
 		} else {
@@ -12,7 +10,15 @@ document.addEventListener('deviceready', function() {
 		}
 	};
 
-	//temporary delay to start for PhoneGap Build troubleshooting purposes
-	var tempStartDelay = setTimeout(start, 5000);
+	var app = document.URL.indexOf( 'http://' ) === -1 && document.URL.indexOf( 'https://' ) === -1;
+	if (app) { // PhoneGap application
+	    document.addEventListener('deviceready', function() { 
+	    	//!!! temporary delay to start for PhoneGap Build troubleshooting purposes
+	    	//Perhaps it would be better to show a start screen anyway, educating about Google Cardboard requirements.
+			var tempStartDelay = setTimeout(start, 5000);
+	    });
+	} else {  // Web page
+		start();
+	}
 
-} , false);
+})();
