aa663faa8372755483b439d71b5bbab1ff8e2747
Christopher Stevens <owntheweb@gmail.com>
2016-02-07 17:16:33 -0700

    updated ship! :D

--- js/Ship2.js
+++ js/Ship2.js
@@ -18,11 +18,35 @@ function Ship2() {
 
 	        if(materials[i].name == 'Ship Paint') {
 	            materials[i] = new THREE.MeshPhongMaterial({
-	                color: 0x777777,
+	                color: 0xffffff,
 	                shading: THREE.FlatShading,
 	                //reflectivity: 0.3,
 	                //envMap: self.cubemap,
 	                name: 'greyMeterial'
+	            });
+	        } else if(materials[i].name == 'Screen') {
+	            materials[i] = new THREE.MeshPhongMaterial({
+	                color: 0x2EA535,
+	                shading: THREE.FlatShading,
+	                //reflectivity: 0.3,
+	                //envMap: self.cubemap,
+	                name: 'greyMeterial'
+	            });
+	        } else if(materials[i].name == 'Seat') {
+	            materials[i] = new THREE.MeshPhongMaterial({
+	                color: 0x5A5A5A,
+	                shading: THREE.FlatShading,
+	                //reflectivity: 0.3,
+	                //envMap: self.cubemap,
+	                name: 'seat'
+	            });
+	        } else if(materials[i].name == 'Can') {
+	            materials[i] = new THREE.MeshPhongMaterial({
+	                color: 0xA02A28,
+	                shading: THREE.FlatShading,
+	                //reflectivity: 0.3,
+	                //envMap: self.cubemap,
+	                name: 'can'
 	            });
 	        } 
 	    }
@@ -42,9 +66,9 @@ function Ship2() {
 			_self.chassis = new THREE.Mesh( geometry, new THREE.MeshFaceMaterial( materials ) );
 			_self.chassis.scale.x = _self.chassis.scale.y = _self.chassis.scale.z = 25;
 			_self.chassis.rotation.y = 0;
-	        _self.chassis.position.x = -5;
-	        _self.chassis.position.y = -2.5;
-	        _self.chassis.position.z = -10;
+	        //_self.chassis.position.x = -5;
+	        //_self.chassis.position.y = -2.5;
+	        //_self.chassis.position.z = -10;
 
 			_self.chassisLoaded = true;
 			_self.areAllModelsLoaded();
