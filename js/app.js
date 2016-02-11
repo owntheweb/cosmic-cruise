df1de2a1a6406aeaeff0d374bed5b7090da4b19f
Christopher Stevens <owntheweb@gmail.com>
2016-02-11 07:00:37 -0700

    Keep app awake with insomnia plugin

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 (function(){
-	
+
 	var start = function() {
     	//show a message if webGL is not supported
 		if(!Detector.webgl) {
@@ -12,6 +12,9 @@
 
 	var app = document.URL.indexOf( 'http://' ) === -1 && document.URL.indexOf( 'https://' ) === -1;
 	if (app) { // PhoneGap application
+	    //keep app awake using insomnia plugin (added in config.xml)
+    	window.plugins.insomnia.keepAwake();
+
 	    document.addEventListener('deviceready', function() { 
 	    	//!!! temporary delay to start for PhoneGap Build troubleshooting purposes
 	    	//Perhaps it would be better to show a start screen anyway, educating about Google Cardboard requirements.
