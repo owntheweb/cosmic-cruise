79c41480384e17ec5fce58195485b122a484299b
Christopher Stevens <owntheweb@gmail.com>
2016-02-14 19:06:36 -0700

    PhoneGap not starting: insomnia plugin not being loaded properly

--- js/app.js
+++ js/app.js
@@ -13,8 +13,7 @@ var onDeviceReady = function() {
 //thanks: http://stackoverflow.com/questions/8068052/phonegap-detect-if-running-on-desktop-browser
 if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|IEMobile)/)) {
 	console.log('running in app...');
-	//window.plugins.insomnia.keepAwake();
-	document.addEventListener("deviceready", onDeviceReady, false);
+	document.addEventListener("deviceready", function() { window.plugins.insomnia.keepAwake(); onDeviceReady(); }, false);
 } else {
 	console.log('running in browser...');
 	onDeviceReady(); //this is the browser
