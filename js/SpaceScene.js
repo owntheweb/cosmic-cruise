a7c1a95d3334e13bdc83e458d0c738aeb3ab09e8
Christopher Stevens <owntheweb@gmail.com>
2016-04-01 13:50:28 -0600

    temporarily re-enable stats

--- js/SpaceScene.js
+++ js/SpaceScene.js
@@ -252,13 +252,13 @@ function SpaceScene(viewMode) {
 		// STATS //
 		///////////
 
-		/*
+		
 		_s.stats = new Stats();
 		_s.stats.domElement.style.position = 'absolute';
 		_s.stats.domElement.style.bottom = '0px';
 		_s.stats.domElement.style.zIndex = 100;
 		_s.container.appendChild( _s.stats.domElement );
-		*/
+		
 
 		////////////
 		// SKYBOX //
@@ -569,7 +569,7 @@ function SpaceScene(viewMode) {
 
 		_s.uncoverVeil();
 
-		//_s.stats.update();
+		_s.stats.update();
 	};
 
 	//render three.js scene
